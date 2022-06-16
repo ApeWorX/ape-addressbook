b330e224deab7b3f5dad9eb41616231cd5896c32
15219900+Ninjagod1251@users.noreply.github.com <15219900+Ninjagod1251@users.noreply.github.com>
2022-06-17 02:49:56 +0700

    feat: upgrade ape

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     url="https://github.com/ApeWorX/ape-addressbook",
     include_package_data=True,
     install_requires=[
-        "eth-ape>=0.2.1,<0.3.0",
+        "eth-ape>=0.3.0,<0.4.0",
         "importlib-metadata ; python_version<'3.8'",
     ],  # NOTE: Add 3rd party libraries here
     python_requires=">=3.7,<4",
